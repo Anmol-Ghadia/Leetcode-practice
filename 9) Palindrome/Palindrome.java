9402c53908ce2239628f9cfac6ed2fbf98bb53d8
AnmolG <anmolghadia1601@gmail.com>
2023-01-02 22:33:59 -0800

    Reviewed solutions

--- 9) Palindrome/Palindrome.java
+++ 9) Palindrome/Palindrome.java
@@ -1,21 +1,39 @@
-
-// 
+// Using int and math
 class Solution {
     public boolean isPalindrome(int x) {
-        char[] y = String.valueOf(x).toCharArray();
-        int len = y.length;
+    if (0>x) {
+        return false;
+    }
+    int oldNum = x;
+    int newNum = 0;
 
-        for (int i=0; i < len; i++) {
-            if (y[i] != y[len - i - 1]) {
+    while (x > 0) {
+        newNum = x%10 + newNum*10;
+        x /= 10;
+    }
+    return (oldNum == newNum);
+    }
+}
+
+// Using String
+class Solution {
+    public boolean isPalindrome(int x) {
+        String y = String.valueOf(x);
+        int front = 0;
+        int end = y.length() -1;
+
+        while (front <= end) {
+            if (y.charAt(front) != y.charAt(end)) {
                 return false;
             }
+            front++;
+            end--;
         }
         return true;
     }
 }
 
-
-// Minor improvement in memory
+// Using Array
 class Solution {
     public boolean isPalindrome(int x) {
         char[] y = String.valueOf(x).toCharArray();
